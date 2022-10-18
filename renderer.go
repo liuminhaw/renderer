8ecd8ac1a76246634897fa1b528f6e02dd882a0a
lmhaw <liuminhaw@gmail.com>
2022-10-19 00:34:30 +0800

    feat: determinve render completion with networkIdle status

--- renderer.go
+++ renderer.go
@@ -6,24 +6,48 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/cdproto/dom"
-	"github.com/chromedp/cdproto/fetch"
-	"github.com/chromedp/cdproto/network"
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
 )
 
+type networkIdle struct {
+	navigateFrame bool
+	frameId       string
+	idleCount     int
+}
+
 // RenderPage rendered given url in browser and returns result html content
 func RenderPage(ctx context.Context, urlStr string) ([]byte, error) {
-	fmt.Printf("ctx headless: %v\n", ctx.Value("headless"))
+	// fmt.Printf("ctx headless: %v\n", ctx.Value("headless"))
+	windowWidth, windowHeight := 1000, 1000
+	idleType := "networkIdle"
 
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("headless", ctx.Value("headless")),
-		chromedp.Flag("blink-settings", fmt.Sprintf("imagesEnbled=%v", ctx.Value("imageLoad"))),
-		chromedp.WindowSize(ctx.Value("windowWidth").(int), ctx.Value("windowHeight").(int)),
-	)
-	fmt.Printf("Rendering: %s\n", urlStr)
+	if v, ok := ctx.Value("idleType").(string); ok {
+		idleType = v
+	}
+
+	var opts = chromedp.DefaultExecAllocatorOptions[:]
+	if _, ok := ctx.Value("headless").(bool); ok {
+		opts = append(
+			opts,
+			chromedp.Flag("headless", ctx.Value("headless")),
+		)
+	}
+	if _, ok := ctx.Value("imageLoad").(bool); ok {
+		opts = append(
+			opts,
+			chromedp.Flag("blink-settings", fmt.Sprintf("imagesEnbled=%v", ctx.Value("imageLoad"))),
+		)
+	}
+	if width, ok := ctx.Value("windowWidth").(int); ok {
+		windowWidth = width
+	}
+	if height, ok := ctx.Value("windowHeight").(int); ok {
+		windowHeight = height
+	}
+	opts = append(opts, chromedp.WindowSize(windowWidth, windowHeight))
+	// fmt.Printf("Rendering: %s\n", urlStr)
 
 	start := time.Now()
 	ctx, cancel := chromedp.NewExecAllocator(ctx, opts...)
@@ -34,7 +58,7 @@ func RenderPage(ctx context.Context, urlStr string) ([]byte, error) {
 	var res string
 	err := chromedp.Run(ctx,
 		chromedp.Tasks{
-			navigateAndWaitFor(urlStr, "InteractiveTime"),
+			navigateAndWaitFor(urlStr, idleType),
 			chromedp.ActionFunc(func(ctx context.Context) error {
 				node, err := dom.GetDocument().Do(ctx)
 				if err != nil {
@@ -55,44 +79,54 @@ func RenderPage(ctx context.Context, urlStr string) ([]byte, error) {
 	}
 
 	duration := time.Since(start)
-	fmt.Println(duration)
+	fmt.Printf("Render time: %v\n", duration)
 	return []byte(res), nil
 }
 
 // navigateAndWaitFor is defined as task of chromedp for rendering step
-func navigateAndWaitFor(url string, eventName string) chromedp.ActionFunc {
+func navigateAndWaitFor(url string, waitType string) chromedp.ActionFunc {
 	return func(ctx context.Context) error {
 		_, _, _, err := page.Navigate(url).Do(ctx)
 		if err != nil {
 			return err
 		}
 
-		return waitFor(ctx, eventName)
+		return waitFor(ctx, waitType)
 	}
 }
 
 // waitFor listens for events in chromedp and stop loading as soon as given event is match
-func waitFor(ctx context.Context, eventName string) error {
-	timeout := ctx.Value("timeout").(int)
+func waitFor(ctx context.Context, waitType string) error {
+	var timeout = 30
+	if t, ok := ctx.Value("timeout").(int); ok {
+		timeout = t
+	}
 	cctx, cancel := context.WithTimeout(ctx, time.Duration(timeout)*time.Second)
+
+	idleCheck := networkIdle{
+		navigateFrame: false,
+		idleCount:     0,
+	}
 	chromedp.ListenTarget(cctx, func(ev interface{}) {
 		switch e := ev.(type) {
-		case *page.EventLifecycleEvent:
-			// fmt.Printf("Event name: %s\n", e.Name)
-			if e.Name == eventName {
-				cancel()
+		case *page.EventFrameNavigated:
+			// fmt.Printf("Navigate ID: %s, Frame ID: %s\n", e.Type, e.Frame.ID)
+			if !idleCheck.navigateFrame {
+				idleCheck.frameId = e.Frame.ID.String()
 			}
-		case *fetch.EventRequestPaused:
-			go func() {
-				c := chromedp.FromContext(ctx)
-				ctx := cdp.WithExecutor(ctx, c.Target)
-
-				if e.ResourceType == network.ResourceTypeImage {
-					fetch.FailRequest(e.RequestID, network.ErrorReasonBlockedByClient).Do(ctx)
-				} else {
-					fetch.ContinueRequest(e.RequestID).Do(ctx)
+			idleCheck.navigateFrame = true
+			idleCheck.idleCount++
+		case *page.EventLifecycleEvent:
+			switch waitType {
+			case "networkIdle":
+				if isNetworkIdle(&idleCheck, e) {
+					cancel()
 				}
-			}()
+			case "InteractiveTime":
+				if isInteractiveTime(e) {
+					cancel()
+				}
+			}
 		}
 	})
 
@@ -101,13 +135,34 @@ func waitFor(ctx context.Context, eventName string) error {
 		if err := ctx.Err(); err != nil {
 			fmt.Printf("waitFor err: %s\n", err)
 		}
-		fmt.Println("ctx done")
+		// fmt.Println("ctx done")
 		return ctx.Err()
 	case <-cctx.Done():
 		if err := cctx.Err(); errors.Is(err, context.DeadlineExceeded) {
 			return fmt.Errorf("waitFor err: %w", err)
 		}
-		fmt.Println("waitFor: cctx done")
+		// fmt.Println("waitFor: cctx done")
 		return nil
 	}
+}
+
+// isNetworkIdle check if networkIdle met complete state.
+// Complete state is met if idleCount becomes 0 or input event frame id is same
+// as the first frame id from EventFrameNavigated
+func isNetworkIdle(n *networkIdle, e *page.EventLifecycleEvent) bool {
+	if e.Name == "networkIdle" && n.navigateFrame {
+		// fmt.Printf("Event name: %s, Frame ID: %s\n", e.Name, e.FrameID)
+		n.idleCount--
+		return n.idleCount == 0 || n.frameId == e.FrameID.String()
+	}
+
+	return false
+}
+
+// isInteractiveTime check if life cycle have met InteractiveTime event.
+func isInteractiveTime(e *page.EventLifecycleEvent) bool {
+	// if e.Name == "InteractiveTime" {
+	// fmt.Printf("Event name: %s, Frame ID: %s\n", e.Name, e.FrameID)
+	// }
+	return e.Name == "InteractiveTime"
 }
